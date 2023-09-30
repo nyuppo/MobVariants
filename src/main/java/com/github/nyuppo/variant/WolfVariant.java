5d5314d5f0943ef487dae0435f07b1c931a9dfcc
nyuppo <thisisnyuppo@gmail.com>
2023-09-30 10:50:25 -0400

    Added ability for sheep variants to specify custom wool

--- src/main/java/com/github/nyuppo/variant/WolfVariant.java
+++ src/main/java/com/github/nyuppo/variant/WolfVariant.java
@@ -1,8 +1,12 @@
 package com.github.nyuppo.variant;
 
+import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.biome.Biome;
+import org.jetbrains.annotations.Nullable;
 
-public class WolfVariant extends MobVariant implements BreedingCombination {
+public class WolfVariant extends MobVariant implements BreedingCombination, SpawnInBiome {
+    @Nullable private final TagKey<Biome> spawnBiomes;
     private final Identifier parent1Identifier;
     private final Identifier parent2Identifier;
 
@@ -10,12 +14,28 @@ public class WolfVariant extends MobVariant implements BreedingCombination {
         super(namespace, id, weight);
         this.parent1Identifier = parent1Identifier;
         this.parent2Identifier = parent2Identifier;
+        this.spawnBiomes = null;
+    }
+
+    public WolfVariant(String namespace, String id, int weight, Identifier parent1Identifier, Identifier parent2Identifier, TagKey<Biome> spawnBiomes) {
+        super(namespace, id, weight);
+        this.parent1Identifier = parent1Identifier;
+        this.parent2Identifier = parent2Identifier;
+        this.spawnBiomes = spawnBiomes;
     }
 
     public WolfVariant(Identifier identifier, int weight, Identifier parent1Identifier, Identifier parent2Identifier) {
         super(identifier, weight);
         this.parent1Identifier = parent1Identifier;
         this.parent2Identifier = parent2Identifier;
+        this.spawnBiomes = null;
+    }
+
+    public WolfVariant(Identifier identifier, int weight, Identifier parent1Identifier, Identifier parent2Identifier, TagKey<Biome> spawnBiomes) {
+        super(identifier, weight);
+        this.parent1Identifier = parent1Identifier;
+        this.parent2Identifier = parent2Identifier;
+        this.spawnBiomes = spawnBiomes;
     }
 
     public Identifier getParent1() {
@@ -28,5 +48,10 @@ public class WolfVariant extends MobVariant implements BreedingCombination {
 
     public boolean validParents(Identifier parent1, Identifier parent2) {
         return parent1.equals(this.parent1Identifier) && parent2.equals(this.parent2Identifier);
+    }
+
+    @Nullable
+    public TagKey<Biome> getSpawnBiomes() {
+        return this.spawnBiomes;
     }
 }
