e8a3a7a29eb39b3c2a25367ffb8c65bd9fcd27a5
nyuppo <thisisnyuppo@gmail.com>
2023-10-01 01:51:08 -0400

    Fixed parent-check being directional

--- src/main/java/com/github/nyuppo/variant/BreedingResultModifier.java
+++ src/main/java/com/github/nyuppo/variant/BreedingResultModifier.java
@@ -1,10 +1,12 @@
 package com.github.nyuppo.variant;
 
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.random.Random;
 
-public record BreedingResultModifier(MobVariant parent1, MobVariant parent2, double breedingChance) implements VariantModifier {
+public record BreedingResultModifier(Identifier parent1, Identifier parent2, double breedingChance) implements VariantModifier {
     public boolean validParents(MobVariant parent1, MobVariant parent2) {
-        return parent1.getIdentifier().equals(this.parent1.getIdentifier()) && parent2.getIdentifier().equals(this.parent2.getIdentifier());
+        return (parent1.getIdentifier().equals(this.parent1) && parent2.getIdentifier().equals(this.parent2))
+                || (parent1.getIdentifier().equals(this.parent2) && parent2.getIdentifier().equals(this.parent1));
     }
 
     public boolean shouldBreed(Random random) {
