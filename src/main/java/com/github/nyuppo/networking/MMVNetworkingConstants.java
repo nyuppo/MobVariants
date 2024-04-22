957c9be7793ec0e861ea14d246a29c1f2c115608
fzzyhmstrs <fzzyhmstrs@gmail.com>
2024-04-22 17:51:42 -0400

    redesign the PacketByteBuf read to be entirely in the Netty Loop

--- src/main/java/com/github/nyuppo/networking/MMVNetworkingConstants.java
+++ src/main/java/com/github/nyuppo/networking/MMVNetworkingConstants.java
@@ -6,4 +6,5 @@ import net.minecraft.util.Identifier;
 public class MMVNetworkingConstants {
     public static final Identifier CLIENT_REQUEST_VARIANT_ID = MoreMobVariants.id("client_request");
     public static final Identifier SERVER_RESPOND_VARIANT_ID = MoreMobVariants.id("server_respond");
+    public static final Identifier SERVER_RESPOND_BASIC_VARIANT_ID = MoreMobVariants.id("server_respond_basic");
 }
